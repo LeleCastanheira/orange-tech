af284460fbc17ab5963fd41cb703b0eb7834df5b
LeleCastanheira <lelecastanheira@hotmail.com>
2022-11-17 18:29:21 -0300

    Aula Variáveis e Operadores

---
+++ Aulas/aula01.js
@@ -0,0 +1,20 @@
+// PRINT
+// console.log('Imprime algo');
+
+
+// DECLARAÇÃO DE VARIÁVEIS
+// let variavel = 10;
+
+// variavel = 20;
+
+// const pi = 3.14;
+
+// console.log(variavel)
+
+// OPERADORES MATEMÁTICOS
+// let variavel = 10 + 10;
+// let variavel = 10 - 10;
+// let variavel = 10 * 10;
+// let variavel = 10 / 10;
+// console.log(variavel)
+
