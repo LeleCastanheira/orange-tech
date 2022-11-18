141bd8664c70be368da465f4db15313ae3ac26dd
LeleCastanheira <lelecastanheira@hotmail.com>
2022-11-17 22:56:44 -0300

    Aula Estruturas Condicionais

---
+++ Aulas/aula02.js
@@ -0,0 +1,28 @@
+//CONDICIONAIS
+//booleano
+//const camisaRenanAzul = true;
+//const camisaRenanVermelha = false;
+
+//10 >= 9 = true;
+//10 < 5 = false;
+
+
+//EXEMPLO
+//verificar se um número é par
+// const numero = 10;
+// const ispar = (numero % 2) === 0;   //is usado para boleanos, mas não é obrigatório
+//console.log(ispar);
+
+
+//IF - ELSE - ELSE IF
+// if(numero === 0) {
+//   console.log('O número é inválido')
+// }else if(par){
+//     console.log('Par!');
+// } else {
+//   console.log('Impar!');
+// }
+//    OU
+// if(!par) {
+//   console.log('Impar!');
+// }
