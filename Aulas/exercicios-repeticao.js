b7858896ab520104c3d477d8ec80d382974e048c
LeleCastanheira <lelecastanheira@hotmail.com>
2022-12-01 14:56:44 -0300

    Praticando estrutura de repetição referente a aula 05

---
+++ Aulas/exercicios-repeticao.js
@@ -0,0 +1,15 @@
+/* 1) Crie um programa que dado um número imprima a sua tabuada. */
+
+// const numero = 2;
+// for (let i = 1; i < 11; i++) {
+//   console.log(`${numero} x ${i} = ${numero*i}`);
+// }
+
+
+/* 2) Crie um programa que seja capaz de percorrer uma lista de números e imprima cada número Par encontrado. */
+// const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 0; i < numeros.length; i++) {
+//   if (numeros[i] % 2 === 0) {
+//     console.log(i);
+//   }
+// }
